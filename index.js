677c3fcc557f0fefd98d2f0f4f20cf542c3fcd8b
Pramod J M <pramodjm4@gmail.com>
2024-05-17 15:30:19 +0530

    adding 3 correct api

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(flash())
 app.use(globalStorage);
 //----------------------------------------------------
 app.use(cors({
-  origin: ["https://shopmore-theta.vercel.app", "http://localhost:3000", "https://shop-more-fe.netlify.app"],
+  origin: ["https://shop-more.vercel.app", "http://localhost:3000", "https://shop-more-fe.netlify.app"],
   credentials: true,
 }))
 //---------------------------------------------------
