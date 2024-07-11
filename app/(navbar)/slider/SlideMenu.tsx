5825f0462e2d951512f2e876a28c613dac753b50
Pramod J M <pramodjm4@gmail.com>
2024-07-11 23:16:13 +0530

    fix home page  UI in mobile view

--- app/(navbar)/slider/SlideMenu.tsx
+++ app/(navbar)/slider/SlideMenu.tsx
@@ -46,9 +46,9 @@ const SlideMenu = () => {
       {loading ? (
         <div
           role="status"
-          className="h-[62vh] md:h-[78vh] w-full space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center  bg-blue-950  mt-[70px] "
+          className="w-full h-[71vh] flex items-center justify-center gap-5  bg-blue-950 mt-[80px] p-7 flex-col md:flex-row"
         >
-          <div className="h-[80vh] md:h-[70vh] hidden md:flex items-center justify-center w-[50%]  bg-gray-300 rounded  dark:bg-gray-700 ml-7">
+          <div className="h-[80vh] md:h-[71vh]  flex items-center justify-center w-full md:w-[50%]  bg-gray-300 rounded  dark:bg-gray-700 ml-0 md:ml-7">
             <svg
               className="w-10 h-10 text-gray-200 dark:text-gray-600"
               aria-hidden="true"
@@ -61,15 +61,15 @@ const SlideMenu = () => {
           </div>
 
           {/* information */}
-          <div className="p-5 w-full md:w-[50%] flex flex-col  justify-center  md:items-start gap-[2%] md:gap-[10%]  h-full">
-            <div className="h-[30px] md:h-[40px] font-semibold w-[30%] bg-gray-200 "></div>
+          <div className="p-0 md:p-5 w-full  md:w-[50%] flex flex-col  justify-center  md:items-start gap-[5px] md:gap-[10%]  h-full">
+            <div className="h-[24px] md:h-[40px] font-semibold w-[30%] bg-gray-200 "></div>
             <div className="w-full">
-              <div className="h-[40px] bg-gray-200 w-[20%]  dark:bg-gray-700  mb-2.5"></div>
-              <div className="h-[40px] bg-gray-200 w-[50%]  dark:bg-gray-700 mb-2.5"></div>
+              <div className="h-[30px] md:h-[40px] bg-gray-200 w-[20%]  dark:bg-gray-700  mb-2.5"></div>
+              <div className="h-[30px] md:h-[40px] bg-gray-200 w-[50%]  dark:bg-gray-700 mb-2.5"></div>
             </div>
             <div className="h-2 bg-gray-200 w-full rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5"></div>
             <div className="h-2 bg-gray-200 w-full rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5"></div>
-            <div className="h-[40px] bg-gray-200 w-[20%]  dark:bg-gray-700  mb-2.5"></div>
+            <div className="h-[30px] md:h-[40px] bg-gray-200 w-[20%]  dark:bg-gray-700  mb-2.5"></div>
           </div>
           <span className="sr-only">Loading...</span>
         </div>
@@ -81,7 +81,7 @@ const SlideMenu = () => {
           showDots={true}
           ssr={true} // means to render carousel on server-side.
           infinite={true}
-          autoPlay={false} //
+          autoPlay={true} //
           autoPlaySpeed={5000}
           keyBoardControl={true}
           customTransition="all .5"
@@ -105,14 +105,14 @@ const SlideMenu = () => {
                 // initial={{ scale: 0.9 }}
                 // transition={{ delay: 1 }}
                 key={index}
-                className="w-full h-[62vh] md:h-[70vh] flex items-center justify-center gap-5  bg-blue-950 mt-[80px] p-7"
+                className="w-full h-[71vh] flex items-center justify-center gap-5  bg-blue-950 mt-[80px] p-7 flex-col md:flex-row"
               >
                 {/* image */}
                 <motion.div
                   animate={{ scale: 1 }}
                   initial={{ scale: 0.8 }}
                   transition={{ delay: 1 }}
-                  className="hidden md:flex w-[50%] h-full items-center justify-center bg-white "
+                  className=" flex w-full  md:w-[50%] h-full items-center justify-center bg-white "
                 >
                   <motion.img
                     src={val.thumbnail?.imageUrl}
@@ -120,7 +120,7 @@ const SlideMenu = () => {
                     initial={{ scale: 0.8 }}
                     transition={{ delay: 1 }}
                     alt=""
-                    className="hidden md:block  h-full "
+                    className=" md:block  h-full md:w-1/2 max-w-[50%] md:max-w-[100%] "
                   />
                 </motion.div>
 
@@ -131,11 +131,12 @@ const SlideMenu = () => {
                   transition={{ delay: 1 }}
                   className="w-full h-auto md:w-[50%] md:h-full flex flex-col  items-start justify-start md:justify-between"
                 >
-                  <h1 className="text-[30px] md:text-[40px] font-semibold text-yellow-400 capitalize">
+                  <h1 className="text-[20px] md:text-[40px] font-semibold text-yellow-400 capitalize">
                     {val.title}
                   </h1>
-                  <h1 className=" text-[40px]  capitalize md:text-[60px] leading-[40px]  md:leading-[70px] text-white font-bold  md:text-left text-justify">
-                    Best <br className="hidden md:block" /> {val.category}{' '}
+                  <h1 className=" text-[30px]  capitalize md:text-[60px] leading-[40px]  md:leading-[70px] text-white font-bold  md:text-left text-justify">
+                    Best
+                    <br className="hidden md:block" /> {val.category}{' '}
                   </h1>
                   <p className="text-[20px] md:text-[20px] font-semibold text-opacity-70 py-3 text-white md:text-left text-justify">
                     {val.description}{' '}
@@ -147,13 +148,29 @@ const SlideMenu = () => {
                       setButtonLoading(true);
                     }}
                   >
-                    {buttonLoading ? <div role="status">
-    <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-    </svg>
-    <span className="sr-only">Loading...</span>
-</div> : 'Take me there'}
+                    {buttonLoading ? (
+                      <div role="status">
+                        <svg
+                          aria-hidden="true"
+                          className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"
+                          />
+                        </svg>
+                        <span className="sr-only">Loading...</span>
+                      </div>
+                    ) : (
+                      'Take me there'
+                    )}
                   </button>
                 </motion.div>
               </motion.div>
